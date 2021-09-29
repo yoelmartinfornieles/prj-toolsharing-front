cd9cad3af75c46402fbcb1a8314249625bc6d060
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-29 19:16:14 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProjectListPage from "./pages/ProjectListPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
+import EditProjectPage from "./pages/EditProjectPage";
+
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import PrivateRoute from "./components/PrivateRoute";    // <== IMPORT
+import AnonRoute from "./components/AnonRoute";        // <== IMPORT
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Switch>      
+        <Route exact path="/" component={HomePage} />
+        
+        {/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
+        <PrivateRoute exact path="/projects" component={ProjectListPage} />
+        <PrivateRoute exact path="/projects/:id" component={ProjectDetailsPage} />
+        <PrivateRoute exact path="/projects/edit/:id" component={EditProjectPage} />
+        
+        <AnonRoute exact path="/signup" component={SignupPage} />
+        <AnonRoute exact path="/login" component={LoginPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ProjectDetailsPage.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import AddTask from "../components/AddTask";
+
+import TaskCard from "../components/TaskCard";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+
+function ProjectDetailsPage (props) {
+  const [project, setProject] = useState(null);
+  const projectId = props.match.params.id;
+  
+  
+  const getProject = () => {
+    // Get the token from the localStorage
+    const storedToken = localStorage.getItem('authToken');
+
+    // Send the token through the request "Authorization" Headers
+    axios
+      .get(
+        `${API_URL}/projects/${projectId}`,
+        { headers: { Authorization: `Bearer ${storedToken}` } }
+      )
+      .then((response) => {
+        const oneProject = response.data;
+        setProject(oneProject);
+      })
+      .catch((error) => console.log(error));
+  };
+  
+  
+  useEffect(()=> {
+    getProject();
+  }, [] );
+
+  
+  return (
+    <div className="ProjectDetails">
+      {project && (
+        <>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </>
+      )}
+
+      
+      <AddTask refreshProject={getProject} projectId={projectId} />          
+
+      { project && project.tasks.map((task) => <TaskCard key={task._id} {...task} /> )} 
+
+      <Link to="/projects">
+        <button>Back to projects</button>
+      </Link>
+          
+      <Link to={`/projects/edit/${projectId}`}>
+        <button>Edit Project</button>
+      </Link>
+      
+    </div>
+  );
+}
+
+export default ProjectDetailsPage;
