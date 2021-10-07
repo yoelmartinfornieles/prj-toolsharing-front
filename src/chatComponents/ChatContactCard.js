e5ce3c159a099d95f7bc8e527ff31feedf61aa41
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-10-07 16:17:16 +0200

    YM - Chat fixed

--- src/chatComponents/ChatContactCard.js
+++ src/chatComponents/ChatContactCard.js
@@ -1,6 +1,9 @@
 function ChatContactCard(props) {
   let {user} = props
-  console.log ("USER: ", user.username);
+  console.log ("USER: ", user.profileImg);
+  
+  if (user.profileImg!==undefined) {
+  
   return (
     <>
         <picture className="user-picture">
@@ -13,6 +16,21 @@ function ChatContactCard(props) {
         </div>
     </>
   );
+
+} else {
+  return (
+    <>
+        <picture className="user-picture">
+            <img src="./deafult chat img.jpeg" alt={`${user.name}`} />
+        </picture>
+        <div className="user-info-container">
+            <div className="user-info">
+                <h4>{user.username}</h4>
+            </div>
+        </div>
+    </>
+  );
+}
 }
 
 export default ChatContactCard
